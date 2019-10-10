166cc10eb73cb521fc8c20493ccbe35790aa09eb
Robson Sousa <robson.sousa21@gmail.com>
2019-10-09 22:15:38 -0300

    Implemented comunication with websocket

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from "react";
-
+import { YellowBox } from "react-native";
 import Routes from "./src/Routes";
+
+YellowBox.ignoreWarnings(["Unrecognized WebSocket"]);
 
 export default function App() {
   return <Routes />;
